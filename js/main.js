9d022ace988a402868cf8e9e9e654d7b26f7ea57
Marat Shagiev <m_js@bk.ru>
2016-09-30 18:37:28 +0300

    get interface values to the object pass this object with interface values to the host

--- js/main.js
+++ js/main.js
@@ -17,14 +17,17 @@
     loadJSX ("json2.js");
 
     $ ("#btn_ok").click (function () {
-      var obj   = {};
-      obj.left  = getInput ('left');
-      obj.right = getInput ('right');
-      obj.vCent = getInput ('v_cent');
-      obj.top   = getInput ('top');
-      obj.bott  = getInput ('bott');
-      obj.hCent = getInput ('h_cent');
-      csInterface.evalScript ('addGrid(' + JSON.stringify (obj) + ')', function (res) {
+      /*      var obj   = {};
+       obj.left  = getInput ('left');
+       obj.right = getInput ('right');
+       obj.vCent = getInput ('v_cent');
+       obj.top   = getInput ('top');
+       obj.bott  = getInput ('bott');
+       obj.hCent = getInput ('h_cent');
+       csInterface.evalScript ('addGrid(' + JSON.stringify (obj) + ')', function (res) {
+       // alert (res);
+       });*/
+      csInterface.evalScript ('addGrid(' + JSON.stringify (getInputs ()) + ')', function (res) {
         // alert (res);
       })
     })
@@ -64,6 +67,65 @@
       }
     }
     return arr;
+  }
+
+  function getInputs () {
+    var o      = {
+          left: [], right: [], v_cent: [[], []], top: [], bott: [], h_cent: [[], []]
+        },
+        i, j, val;
+    var left   = document.getElementById ('left').getElementsByTagName ('input'),
+        right  = document.getElementById ('right').getElementsByTagName ('input'),
+        v_cent = document.getElementById ('v_cent').getElementsByTagName ('input'),
+        top    = document.getElementById ('top').getElementsByTagName ('input'),
+        bott   = document.getElementById ('bott').getElementsByTagName ('input'),
+        h_cent = document.getElementById ('h_cent').getElementsByTagName ('input');
+
+    for (i = 0; i < left.length; i++) {
+      val = left[i].value;
+      if (!val) continue;
+      o.left.push (val);
+    }
+    for (i = 0; i < top.length; i++) {
+      val = top[i].value;
+      if (!val) continue;
+      o.top.push (val);
+    }
+    for (i = right.length - 1; i >= 0; i--) {
+      val = right[i].value;
+      if (!val) continue;
+      o.right.push (val);
+    }
+    for (i = bott.length - 1; i >= 0; i--) {
+      val = bott[i].value;
+      if (!val) continue;
+      o.bott.push (val);
+    }
+    {
+      for (i = v_cent.length / 2 - 1; i >= 0; i--) {
+        val = v_cent[i].value;
+        if (!val) continue;
+        o.v_cent[0].push (val);
+      }
+      for (i = v_cent.length / 2; i < v_cent.length; i++) {
+        val = v_cent[i].value;
+        if (!val) continue;
+        o.v_cent[1].push (val);
+      }
+    }
+    {
+      for (i = h_cent.length / 2 - 1; i >= 0; i--) {
+        val = h_cent[i].value;
+        if (!val) continue;
+        o.h_cent[0].push (val);
+      }
+      for (i = h_cent.length / 2; i < h_cent.length; i++) {
+        val = h_cent[i].value;
+        if (!val) continue;
+        o.h_cent[1].push (val);
+      }
+    }
+    return o;
   }
 
   function isNum (n) {
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -2,14 +2,17 @@
 /*global $, Folder*/
 
 function addGrid (obj) {
+  scrollWin (showObjDeep (obj));
 
-  scrollWin (
-    obj.left + '\n' + obj.right + '\n' + obj.vCent + '\n' +
-    obj.top + '\n' + obj.bott + '\n' + obj.hCent
-  );
+  /*  scrollWin (
+   obj.left + '\n' + obj.right + '\n' + obj.vCent + '\n' +
+   obj.top + '\n' + obj.bott + '\n' + obj.hCent
+   );*/
   return 8;
 }
-
+/**
+ * TEST SECTION
+ * */
 function scrollWin (input) {
   if (input instanceof Array)     input = input.join ("\r");
 
@@ -21,5 +24,30 @@ function scrollWin (input) {
 
   w.add ("button", undefined, "Close", {name: "ok"});
   w.show ();
+}
+function showObjDeep (obj) {
+  var str    = '{\n';
+  var indent = 1;
+
+  showObj (obj);
+
+  function showObj (obj) {
+
+    for (var key in obj) {
+      if (typeof obj[key] == 'object' /*&& !obj[key].splice*/) {
+        str += addIndent (indent) + key + ':\n';
+        ++indent;
+        showObj (obj[key]);
+      } else {
+        str += addIndent (indent) + key + ': ' + obj[key] + ' [' + typeof obj[key] + '],\n';
+      }
+    }
+    indent--;
+  }
+
+  return str + '}';
+  function addIndent (i) {
+    return new Array (i).join ('_');
+  }
 }
 
