bb495ff2d7c10b24753f777e0851758b415c82c6
Marat Shagiev <m_js@bk.ru>
2016-10-01 18:19:45 +0300

    v0.5 prereliease usable add description host: add const PT_TO_MM; complete add all functions; set path parameters (stroke 0.15, black 88.88, dash 0.8) little modify interface (buttons) todo: link some fields to mirror modify (pictures or checkboxes) todo: add two units type - pt or mm (radiobutton) todo: add saving last parameters (button save) todo: add color, dashed, stroke width settings (button settings)

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 #Add grid-lines inside selected rectangle
 ##HTML extension panel for Adobe Illustrator
+##Description:
+* Enter values in the respective fields and click `Create a grid!`.
+* Each successive line of a grid is added a **RELATIVELY** the previous one.
+* The `Left` and `Top` fields: lines start from the left or top side of the selected object accordingly.
+* The `Right` and `Bottom` fields: lines start from the right or bottom side of the selected object accordingly.
+<br>You must enter values in the input fields from right to left.
+* The `Center` fields: lines start from the center of the selected object to the both directions of the center accordingly input values.
+<br>You must add value starting from the center to the appropriate side.
+* All values in millimeters
+![Panel](img/ai_grid_panel.png)
+![Panel](img/ai_grid_gridlines.png)
--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -1,14 +1,15 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global $, Folder*/
+var PT_TO_MM = 2.834645668;
 
 function addGrid (obj) {
   // scrollWin (showObjDeep (obj));
   if (!documents.length) {
-    alert ('Needs document and selected rectangle');
+    alert ('Need:\n1.\tDocument\n2.\tSelected rectangle.');
     return;
   }
   if (!selection.length) {
-    alert ('Select rectangle and try again');
+    alert ('Need the selected rectangle.');
     return;
   }
 
@@ -21,47 +22,73 @@ function addGrid (obj) {
   if (obj.left.length) {
     x = rect [0];
     for (i = 0; i < obj.left.length; i++) {
-      x += +obj.left[i];
+      x += +obj.left[i] * PT_TO_MM;
       _addLine ([[x, rect[1]], [x, rect[3]]]);
     }
   }
   if (obj.top.length) {
     y = rect[1];
     for (i = 0; i < obj.top.length; i++) {
-      y -= obj.top[i];
+      y -= obj.top[i] * PT_TO_MM;
       _addLine ([[rect[0], y], [rect[2], y]]);
     }
   }
   if (obj.right.length) {
     x = rect [2];
     for (i = 0; i < obj.right.length; i++) {
-      x -= obj.right[i];
+      x -= obj.right[i] * PT_TO_MM;
       _addLine ([[x, rect[1]], [x, rect[3]]]);
     }
   }
   if (obj.bott.length) {
     y = rect[3];
     for (i = 0; i < obj.bott.length; i++) {
-      y += +obj.bott[i];
+      y += +obj.bott[i] * PT_TO_MM;
       _addLine ([[rect[0], y], [rect[2], y]]);
     }
   }
   if (obj.v_cent[0].length) {
-    // alert (obj.v_cent[0]);
+    x = rect[0] + (rect[2] - rect[0]) / 2;
+    for (i = 0; i < obj.v_cent[0].length; i++) {
+      x -= obj.v_cent[0][i] * PT_TO_MM;
+      _addLine ([[x, rect[1]], [x, rect[3]]]);
+    }
   }
   if (obj.v_cent[1].length) {
-    // alert (obj.v_cent[1]);
+    x = rect[0] + (rect[2] - rect[0]) / 2;
+    for (i = 0; i < obj.v_cent[1].length; i++) {
+      x += +obj.v_cent[1][i] * PT_TO_MM;
+      _addLine ([[x, rect[1]], [x, rect[3]]]);
+    }
   }
   if (obj.h_cent[0].length) {
-    // alert (obj.h_cent[0]);
+    y = rect[1] + (rect[3] - rect[1]) / 2;
+    for (i = 0; i < obj.h_cent[0].length; i++) {
+      y += +obj.h_cent[0][i] * PT_TO_MM;
+      _addLine ([[rect[0], y], [rect[2], y]]);
+    }
   }
   if (obj.h_cent[1].length) {
-    // alert (obj.h_cent[1]);
+    y = rect[1] + (rect[3] - rect[1]) / 2;
+    for (i = 0; i < obj.h_cent[1].length; i++) {
+      y -= obj.h_cent[1][i] * PT_TO_MM;
+      _addLine ([[rect[0], y], [rect[2], y]]);
+    }
   }
 
   function _addLine (pnts) {
+
     pth = lay.pathItems.add ();
     pth.setEntirePath (pnts);
+
+    var pthCol       = new CMYKColor ();
+    pthCol.black     = 88.88;
+    pth.filled       = false;
+    pth.stroked      = true;
+    pth.strokeWidth  = 0.15 * PT_TO_MM;
+    pth.strokeDashes = [0.8 * PT_TO_MM];
+    pth.strokeJoin   = StrokeJoin.BEVELENDJOIN;
+    pth.strokeColor  = pthCol;
   }
 
   return 8;
