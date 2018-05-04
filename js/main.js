cde4a48b5615a63418c16ffcc2f683524a0f8cf2
Marat Shagiev <m_js@bk.ru>
2018-05-04 13:35:48 +0300

    fix bug when input commas and periods including second unnecessary

--- js/main.js
+++ js/main.js
@@ -4,29 +4,29 @@
 (function () {
   'use strict';
 
-  var csInterface = new CSInterface ();
+  var csInterface = new CSInterface();
 
-  function loadJSX (fileName) {
-    var extensionRoot = csInterface.getSystemPath (SystemPath.EXTENSION) + "/jsx/";
-    csInterface.evalScript ('$.evalFile("' + extensionRoot + fileName + '")');
+  function loadJSX(fileName) {
+    var extensionRoot = csInterface.getSystemPath(SystemPath.EXTENSION) + '/jsx/';
+    csInterface.evalScript('$.evalFile("' + extensionRoot + fileName + '")');
   }
 
-  function init () {
+  function init() {
 
-    themeManager.init ();
-    loadJSX ("json2.js");
+    themeManager.init();
+    loadJSX('json2.js');
 
-    $ ("#btn_ok").click (function () {/**/
-      csInterface.evalScript ('addGrid(' + JSON.stringify (getInputs ()) + ')', function (res) {
-      })
+    $('#btn_ok').click(function () {/**/
+      csInterface.evalScript('addGrid(' + JSON.stringify(getInputs()) + ')', function (res) {
+      });
     });
 
     /**
      * Handling the incorrect input
      * */
-    var inputElems = document.getElementsByTagName ('input');
+    var inputElems = document.getElementsByTagName('input');
     for (var i = 0; i < inputElems.length; i++) {
-      var obj        = inputElems[i];
+      var obj = inputElems[i];
       obj.onkeypress = function (e) {
         // keypress keyCode=44 which=44 charCode=44 char=,
         // keypress keyCode=46 which=46 charCode=46 char=.
@@ -35,27 +35,28 @@
           return false;
         }
         if (e.keyCode == 44 || e.keyCode == 46) {
-          if (this.value.match (/[.,]/)) return false;
-          this.value += '.';
-          return false;
+          if (this.value.match(/[.,]/)) {
+            // this.value += '.';
+            return false;
+          }
         }
-      }
+      };
     }
 
-    $ ("#img_github").click (function () {
-      window.cep.util.openURLInDefaultBrowser ("https://github.com/dumbm1/ai_grid");
+    $('#img_github').click(function () {
+      window.cep.util.openURLInDefaultBrowser('https://github.com/dumbm1/ai_grid');
     });
-    $ ("#btn_clear").click (reloadPanel);
-    $ ("#btn_close").click (function () {
-      csInterface.closeExtension ();
+    $('#btn_clear').click(reloadPanel);
+    $('#btn_close').click(function () {
+      csInterface.closeExtension();
     });
 
   }
 
-  init ();
+  init();
 
-  function reloadPanel () {
-    location.reload ();
+  function reloadPanel() {
+    location.reload();
   }
 
   /**
@@ -67,61 +68,61 @@
    * @param {String) setID - container ID that contains the input fields,
    * @return {Array} inputValues - values of input fields (string-numbers)
    * */
-  function getInputs () {
-    var o      = {
+  function getInputs() {
+    var o = {
           left: [], right: [], v_cent: [[], []], top: [], bott: [], h_cent: [[], []]
         },
         i, j, val;
-    var left   = document.getElementById ('left').getElementsByTagName ('input'),
-        right  = document.getElementById ('right').getElementsByTagName ('input'),
-        v_cent = document.getElementById ('v_cent').getElementsByTagName ('input'),
-        top    = document.getElementById ('top').getElementsByTagName ('input'),
-        bott   = document.getElementById ('bott').getElementsByTagName ('input'),
-        h_cent = document.getElementById ('h_cent').getElementsByTagName ('input');
+    var left   = document.getElementById('left').getElementsByTagName('input'),
+        right  = document.getElementById('right').getElementsByTagName('input'),
+        v_cent = document.getElementById('v_cent').getElementsByTagName('input'),
+        top    = document.getElementById('top').getElementsByTagName('input'),
+        bott   = document.getElementById('bott').getElementsByTagName('input'),
+        h_cent = document.getElementById('h_cent').getElementsByTagName('input');
 
     for (i = 0; i < left.length; i++) {
-      pushVal (left[i].value, o.left);
+      pushVal(left[i].value, o.left);
     }
     for (i = 0; i < top.length; i++) {
-      pushVal (top[i].value, o.top);
+      pushVal(top[i].value, o.top);
     }
     for (i = right.length - 1; i >= 0; i--) {
-      pushVal (right[i].value, o.right);
+      pushVal(right[i].value, o.right);
     }
     for (i = bott.length - 1; i >= 0; i--) {
-      pushVal (bott[i].value, o.bott);
+      pushVal(bott[i].value, o.bott);
     }
     {
       for (i = v_cent.length / 2 - 1; i >= 0; i--) {
-        pushVal (v_cent[i].value, o.v_cent[0]);
+        pushVal(v_cent[i].value, o.v_cent[0]);
       }
       for (i = v_cent.length / 2; i < v_cent.length; i++) {
-        pushVal (v_cent[i].value, o.v_cent[1]);
+        pushVal(v_cent[i].value, o.v_cent[1]);
       }
     }
     {
       for (i = h_cent.length / 2 - 1; i >= 0; i--) {
-        pushVal (h_cent[i].value, o.h_cent[0]);
+        pushVal(h_cent[i].value, o.h_cent[0]);
       }
       for (i = h_cent.length / 2; i < h_cent.length; i++) {
-        pushVal (h_cent[i].value, o.h_cent[1]);
+        pushVal(h_cent[i].value, o.h_cent[1]);
       }
     }
     return o;
   }
 
-  function isNum (n) {
-    return !isNaN (parseFloat (n)) && isFinite (n);
+  function isNum(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
   /**
    * immediate change val and arr
    * */
-  function pushVal (val, arr) {
+  function pushVal(val, arr) {
     if (!val) return;
-    val = val.replace (/,/gmi, '.');
-    if (!isNum (val)) return;
-    arr.push (val);
+    val = val.replace(/,/gmi, '.');
+    if (!isNum(val)) return;
+    arr.push(val);
   }
 
-} ());
+}());
